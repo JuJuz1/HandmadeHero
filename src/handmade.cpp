d45159dd48eb595de5d14262b9fa8905bb5562be
Juuso Piippo <juuso.piippo1@gmail.com>
2026-03-17 23:15:26 +0200

    Remove keyboard and mouse debug code

--- src/handmade.cpp
+++ src/handmade.cpp
@@ -49,34 +49,6 @@ OutputSound(GameState* gameState, const SoundOutputBuffer* buff) {
 }
 
 INTERNAL void
-DrawGradient(const OffScreenBuffer* screenBuff, u32 xOffset, u32 yOffset) {
-    // NOTE: maybe see what the optimizer does to buff (passing by value vs pointer)
-    // remember to not get fixated on micro-optimizations before actually doing optimization
-    // though...
-
-    // Pitch (length width-wise)
-    u8* row{ static_cast<u8*>(screenBuff->memory) };
-
-    // Drawing
-    for (i32 y{ 0 }; y < screenBuff->height; ++y) {
-        u32* pixel{ reinterpret_cast<u32*>(row) };
-        for (i32 x{ 0 }; x < screenBuff->width; ++x) {
-            // Windows flipped the order of rbg
-            // Memory: BB GG RR xx
-            // !little endianness!
-
-            const u8 blue{ static_cast<u8>(x + xOffset) };
-            const u8 green{ static_cast<u8>(y + yOffset) };
-
-            // Register: xx RR GG BB
-            *pixel++ = (green << 8) | blue;
-        }
-
-        row += screenBuff->pitch;
-    }
-}
-
-INTERNAL void
 RenderPlayer(const OffScreenBuffer* screenBuff, u32 playerPosX, u32 playerPosY, u32 color) {
     constexpr u32 playerDimension{ 16 }; // Width and height
 
@@ -100,32 +72,8 @@ RenderPlayer(const OffScreenBuffer* screenBuff, u32 playerPosX, u32 playerPosY, 
 
 INTERNAL void
 InitializeGameState(GameState* gameState, GameMemory* memory, ThreadContext* threadContext) {
-    // The memory is already zeroed
-    //gameState->xOffset = 0;
-    //gameState->yOffset = 0;
-    gameState->toneHz = 256;
-    //gameState->tSine = 0;
-    gameState->playerPosX = 200;
-    gameState->playerPosY = 100;
-
-    const char* fileName{ __FILE__ };
-    const auto readResult{ memory->DEBUGReadFile(threadContext, fileName) };
-    if (readResult.content) {
-        memory->DEBUGWriteFile(threadContext, "test.out", readResult.content,
-                               readResult.contentSize);
-        memory->DEBUGFreeFileMemory(threadContext, readResult.content);
-    }
-
     // TODO: maybe make platform set this
     memory->isInitialized = true;
-}
-
-// NOTE: use extern "C" to avoid name mangling
-extern "C" GET_SOUND_SAMPLES(GetSoundSamples) {
-    UNUSED_PARAMS(threadContext);
-
-    GameState* gameState{ static_cast<GameState*>(memory->permanentStorage) };
-    OutputSound(gameState, soundBuff);
 }
 
 extern "C" UPDATE_AND_RENDER(UpdateAndRender) {
@@ -145,78 +93,14 @@ extern "C" UPDATE_AND_RENDER(UpdateAndRender) {
 
     // NOTE: if many players -> loop through input->playerInputs
     const InputButtons* input0Keyboard{ &input->playerInputs[0] };
+}
 
-    constexpr f32 gradientOffset{ 50.0f };
+// NOTE: use extern "C" to avoid name mangling
+extern "C" GET_SOUND_SAMPLES(GetSoundSamples) {
+    UNUSED_PARAMS(threadContext);
 
-    f32 playerVelocityX{}, playerVelocityY{};
-    constexpr f32 playerMoveAmount{ 80.0f };
-    constexpr f32 playerMoveShiftModifier{ 4.0f };
-
-    if (input::ActionPressed(&input0Keyboard->up)) {
-        gameState->yOffset -= gradientOffset * delta;
-        playerVelocityY -= playerMoveAmount;
-    }
-    if (input::ActionPressed(&input0Keyboard->down)) {
-        gameState->yOffset += gradientOffset * delta;
-        playerVelocityY += playerMoveAmount;
-    }
-    if (input::ActionPressed(&input0Keyboard->left)) {
-        gameState->xOffset -= gradientOffset * delta;
-        playerVelocityX -= playerMoveAmount;
-    }
-    if (input::ActionPressed(&input0Keyboard->right)) {
-        playerVelocityX += playerMoveAmount;
-    }
-    // Shift
-    if (input::ActionPressed(&input0Keyboard->shift)) {
-        playerVelocityX *= playerMoveShiftModifier;
-        playerVelocityY *= playerMoveShiftModifier;
-    }
-
-    gameState->playerPosX += playerVelocityX * delta;
-    gameState->playerPosY += playerVelocityY * delta;
-
-    constexpr u32 toneHzOffset{ 30 };
-    //memory->DEBUGPrintInt(threadContext, "toneHz", gameState->toneHz);
-
-    if (input::ActionJustPressed(&input0Keyboard->E)) {
-        gameState->toneHz += toneHzOffset;
-    } else if (input::ActionJustPressed(&input0Keyboard->Q)) {
-        if (gameState->toneHz > toneHzOffset) {
-            gameState->toneHz -= toneHzOffset;
-        } else {
-            gameState->toneHz = 1;
-        }
-    }
-
-    // TODO: allow sample offsets
-    //OutputSound(soundBuff, gameState->toneHz);
-
-    DrawGradient(screenBuff, gameState->xOffset, gameState->yOffset);
-    RenderPlayer(screenBuff, static_cast<u32>(gameState->playerPosX),
-                 static_cast<u32>(gameState->playerPosY), 0xFFFFFFFF);
-    // Mouse
-    RenderPlayer(screenBuff, input->mouseX, input->mouseY, 0xFFFF2222);
-
-    for (u32 i{}; i < ARRAY_COUNT(input->mouseButtons.buttons); ++i) {
-        RenderPlayer(screenBuff, input->mouseX + (i * 20), input->mouseY - (i * 10), i * 20000);
-    }
-
-    if (input::ActionPressed(&input->mouseButtons.left)) {
-        RenderPlayer(screenBuff, input->mouseX + 100, input->mouseY, 0xFF11111);
-    }
-    if (input::ActionPressed(&input->mouseButtons.middle)) {
-        RenderPlayer(screenBuff, input->mouseX, input->mouseY - 200, 0x991199);
-    }
-    if (input::ActionPressed(&input->mouseButtons.right)) {
-        RenderPlayer(screenBuff, input->mouseX + 500, input->mouseY + 100, 0xFF55555);
-    }
-    if (input::ActionPressed(&input->mouseButtons.x1)) {
-        RenderPlayer(screenBuff, input->mouseX - 250, input->mouseY + 100, 0xFF55555);
-    }
-    if (input::ActionPressed(&input->mouseButtons.x2)) {
-        RenderPlayer(screenBuff, input->mouseX + 75, input->mouseY + 100, 0xFF55555);
-    }
+    GameState* gameState{ static_cast<GameState*>(memory->permanentStorage) };
+    OutputSound(gameState, soundBuff);
 }
 
 } //namespace game
